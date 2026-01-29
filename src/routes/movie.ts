54cc123a4f36909c785714f84a0e4b9692235670
tame84 <146751584+tame84@users.noreply.github.com>
2026-01-29 17:07:01 +0100

    feat: add an endpoint to retrieves all shows date for a movie

--- src/routes/movie.ts
+++ src/routes/movie.ts
@@ -105,6 +105,16 @@ const app = new Hono()
 
             return c.json(showtimes[0]);
         },
-    );
+    )
+    .get("/shows/dates", async (c) => {
+        const slug = c.req.param("slug");
+
+        const dates = await db
+            .selectDistinct({ date: showsTable.date })
+            .from(moviesTable)
+            .where(eq(moviesTable.slug, slug))
+            .innerJoin(showsTable, eq(showsTable.movieUuid, moviesTable.uuid));
+        return c.json(Array.from(dates.flatMap((d) => d.date)).sort((a, b) => a.getTime() - b.getTime()));
+    });
 
 export default app;
