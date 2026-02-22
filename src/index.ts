5017665b00ea480b6ed332eacafcce03671b7efc
tame84 <146751584+tame84@users.noreply.github.com>
2026-02-22 13:19:50 +0100

    fix: remove old endpoints routes

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,6 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import scrapingRoutes from "./routes/scrape";
-import showsRoutes from "./routes/shows";
-import movieRoutes from "./routes/movie";
-import genresRoutes from "./routes/genres";
-import versionsRoutes from "./routes/versions";
-import cinemasRoutes from "./routes/cinemas";
 import { customLogger } from "src/utils/logger";
 
 const app = new Hono()
@@ -16,12 +11,7 @@ const app = new Hono()
         console.error(err);
         return c.text("Internal Server Error", 500);
     })
-    .route("/scrape", scrapingRoutes)
-    .route("/shows", showsRoutes)
-    .route("/movie", movieRoutes)
-    .route("/genres", genresRoutes)
-    .route("/versions", versionsRoutes)
-    .route("/cinemas", cinemasRoutes);
+    .route("/scrape", scrapingRoutes);
 
 serve(
     {
