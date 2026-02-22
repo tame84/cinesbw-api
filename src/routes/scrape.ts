6e976283ee108b8240ae521933f957779c2c02ba
tame84 <146751584+tame84@users.noreply.github.com>
2026-02-22 18:54:00 +0100

    fix: remove old code

--- src/routes/scrape.ts
+++ src/routes/scrape.ts
@@ -76,12 +76,6 @@ interface TmdbMovieDetailsResponse {
 
 const CINENEWS_BASE_URL = "https://www.cinenews.be";
 
-const generateUserAgent = () => {
-    const ua = new UserAgent(/Chrome/);
-    return ua.toString();
-};
-
-// CE CODE FONCTIONNE POTENTILLEMENT EN PROD (ATTENDRE QUELQUES JOURS POUR AVOIR DES RETOURS)
 const USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
